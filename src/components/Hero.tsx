7163625b7869c0f0e2d41ced33b755a9b8998fc3
Lag0 <brunolago@incorppar.com>
2024-04-28 20:50:29 -0300

    feat: fixing ai message not showing up

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -56,9 +56,7 @@ const Hero = () => {
                   height={490}
                 />
 
-                <ScrollParallax>
-                  <Generating className="absolute bottom-5 left-4 right-4 md:bottom-8 md:left-1/2 md:right-auto md:w-[31rem] md:-translate-x-1/2" />
-                </ScrollParallax>
+                <Generating className="absolute bottom-5 left-4 right-4 md:bottom-8 md:left-1/2 md:right-auto md:w-[31rem] md:-translate-x-1/2" />
 
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="absolute -left-[5.5rem] bottom-[7.5rem] hidden rounded-2xl border border-n-1/10 bg-n-9/40 px-1 py-1 backdrop-blur xl:flex">
